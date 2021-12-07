8bb1986c39eab85c9607ba351b37923e28a66f11
kaco0307 <pjelfenomeno15@gmail.com>
2021-12-07 03:02:39 -0500

    base de datos

--- basedb.sql
+++ basedb.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
-<<<<<<< HEAD
--- Tiempo de generación: 06-12-2021 a las 19:32:58
-=======
--- Tiempo de generación: 05-12-2021 a las 04:05:27
->>>>>>> master
+-- Tiempo de generación: 07-12-2021 a las 09:01:08
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.5
 
@@ -77,7 +73,36 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2021_12_02_012133_create_mascotas_table', 2),
 (6, '2021_12_02_012116_create_admins_table', 3),
-(7, '2014_10_12_000000_create_users_table', 4);
+(9, '2014_10_12_000000_create_users_table', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cedula` int(11) NOT NULL,
+  `Fecha_naci` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `cedula`, `Fecha_naci`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'ethan', 1192788415, NULL, 'cuentaethanha@gmail.com', NULL, '$2y$10$tOVpuI33XUFMIEyzRCHYlOeA1c3j9zeiGq3WGYrnjNvIpYI86A.mO', NULL, '2021-12-06 05:54:25', '2021-12-06 05:54:25'),
+(5, 'andrea', 1111564616, '2009-06-17', 'andrea@gmail.com', NULL, '$2y$10$nkReMQPPd8skYINiLrlNt.FNmIuyQ/X8KM4U42D0YY4RZn06Xbqpa', NULL, '2021-12-06 06:03:30', '2021-12-06 06:03:30'),
+(6, 'jose', 52631489, '2000-08-03', 'pjelfenomeno15@gmail.com', NULL, '$2y$10$L4fU/q36pOqCDWkVnNS8tesO4JMwconGxeTbmyhJ5Ll4qlbU2eUja', NULL, '2021-12-07 11:34:54', '2021-12-07 11:34:54'),
+(7, 'carlitos', 1564841649, '1996-05-04', 'carlitos@gmail.com', NULL, '$2y$10$Ch1FFqNAdbcLxrsJhN8auOjqaAEkSNyU0WkuCrZxqzr3BcUmUujs6', NULL, '2021-12-07 12:27:21', '2021-12-07 12:51:09');
 
 --
 -- Índices para tablas volcadas
@@ -102,6 +127,13 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -121,7 +153,13 @@ ALTER TABLE `mascotas`
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
